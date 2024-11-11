d6ed25e16b3d395bce22a069281cabdc7e54cc31
Rina <doroshukmaryna@gmail.com>
2024-11-11 21:51:39 +0100

    fixed up structure and added 2 pages

---
+++ frontend/src/js/Home.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { fetchMessage } from './fetcher';
+// import '../css/Home.css';
+
+function Home() {
+    const [message, setMessage] = useState("");
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const message = await fetchMessage();
+          setMessage(message);
+        } catch (error) {
+          console.error("Error fetching message:", error);
+        }
+      };
+  
+      fetchData();
+    }, []);
+  
+    return (
+      <div className="Home">
+        <h1>FastAPI React Integration</h1>
+        <p>{message}</p>
+      </div>
+    );
+  }
+  
+  export default Home;
