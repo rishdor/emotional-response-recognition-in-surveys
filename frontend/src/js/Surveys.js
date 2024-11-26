879ba73147a4e4d950b3b80477e7c62a1938e71c
sylsocha <sylwia9socha@gmail.com>
2024-11-26 23:51:08 +0100

    Surveys

--- frontend/src/js/Surveys.js
+++ frontend/src/js/Surveys.js
@@ -1,10 +1,52 @@
 import React from 'react';
+import '../css/Dashboard.css';
+import '../css/Surveys.css';
 
 function Surveys() {
   return (
     <div className="Surveys">
-      <h2>Surveys</h2>
-      <p>This is the Surveys page.</p>
+      <h1>all surveys in one place</h1> {/* Replace NAME with the user's name */}
+      <div class='sidebar'>
+        <ul>
+            <li><h3>NAVIGATE</h3></li>
+            <li><a href="#in_progress">in progress</a></li>
+            <li><a href="#new">new</a></li>
+        </ul>
+      </div>
+
+      <div class='section'>
+        <h2 class='section_name' id='surveys'>in progress</h2>
+        <hr class='devide_line'></hr>
+        <div class='survey_list'>
+          <div class='survey_headers'>
+            <p>survey name</p>
+            <p class='deadline'>deadline</p>
+          </div>
+
+          <div class='survey_cont_surv'>
+            <p class='sur_name'>survey name</p> {/* Replace message with the name of the survey */}
+            <p class='deadline'>deadline</p> {/* Replace date with the date */}
+            <button>start</button>
+          </div>
+        </div>
+      </div>
+
+      <div class='section'>
+        <h2 class='section_name' id='surveys'>new</h2>
+        <hr class='devide_line'></hr>
+        <div class='survey_list'>
+          <div class='survey_headers'>
+            <p>survey name</p>
+            <p class='deadline'>deadline</p>
+          </div>
+
+          <div class='survey_cont_surv'>
+            <p class='sur_name'>survey name</p> {/* Replace message with the name of the survey */}
+            <p class='deadline'>deadline</p> {/* Replace date with the date */}
+            <button>start</button>
+          </div>
+        </div>
+      </div>
 
       <footer>
       </footer>
