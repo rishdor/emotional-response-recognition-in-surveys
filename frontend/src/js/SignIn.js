f291d35ae318ab1f351e85d2528b743db26f2936
sylsocha <sylwia9socha@gmail.com>
2024-11-26 12:00:55 +0100

    sign in and sign up form set up

--- frontend/src/js/SignIn.js
+++ frontend/src/js/SignIn.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import '../css/SignForm.css';
 
 function SignIn() {
   return (
     <div className="SignIn">
-      <h2>SignIn</h2>
-      <p>This is the SignIn page.</p>
+      <h1>Sign In</h1>
+
+      <div class='signForm'>
+        <div class='formElem'>
+          <label for="email">email:</label>
+          <input type="email" name='email' required />
+        </div>
+
+        <div class='formElem'>
+          <label for="pass">password:</label>
+          <input type="password" name='pass' required />
+        </div>  
+
+        <div class='formElem'>
+          <input type="submit" value="Sign In" />
+        </div>
+      </div>
     </div>
   );
 }
