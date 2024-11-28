c6703de771b60edb44dbe835def2e8d94554b5df
sylsocha <sylwia9socha@gmail.com>
2024-11-28 22:38:26 +0100

    Thank you page

--- frontend/src/js/ThankYou.js
+++ frontend/src/js/ThankYou.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../css/ThankYou.css';
+import '../css/SurveyWindow.css';
+import thankULeft from '../images/photos/thank_you_left.png'
+import thankURight from '../images/photos/thank_you_right.png'
 
 function ThankYou() {
   return (
     <div className="ThankYou">
-      <h2>ThankYou</h2>
+      <div class='thank_container'>
+        <img src={thankULeft} alt='Thank you' class='thx_img1'/>
+        <h1 class='thx_message'>thank you for being a part of this survey</h1>
+        <img src={thankURight} alt='Thank you' class='thx_img2'/>
+      </div>
+      <div class='finish'>
+        <Link to="/surveys" class='link'><input type="submit" value="go back to surveys" /></Link>
+      </div>
     </div>
   );
 }
