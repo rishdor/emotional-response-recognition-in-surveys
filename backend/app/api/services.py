5e1683e988aa54e3cfe7071b06335e2b1fa50d03
Kacper <fexi1339@gmail.com>
2024-11-29 14:32:05 +0100

    setting cookies and navigation flow

--- backend/app/api/services.py
+++ backend/app/api/services.py
@@ -1,9 +1,11 @@
 from sqlalchemy.orm import Session
 from database.user import User
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, Response
 from database.schemas import UserLogin
+import secrets
+from datetime import datetime, timedelta, timezone
 
-def login_user(db: Session, user: UserLogin):
+def login_user(db: Session, user: UserLogin, response: Response):
     user_db = db.query(User).filter(User.email == user.email).first()
 
     if user_db is None:
@@ -18,4 +20,15 @@ def login_user(db: Session, user: UserLogin):
             detail="Invalid credentials",
         )
     
+    session_token = secrets.token_hex(16)
+    
+    expires_at = datetime.utcnow().replace(tzinfo=timezone.utc) + timedelta(minutes=30) 
+    response.set_cookie(
+        key="user_session",
+        value=session_token,
+        expires=expires_at,
+        httponly=False,       
+        samesite="Strict",  
+    )
+
     return {"message":"Login successful", "user": user.email}
