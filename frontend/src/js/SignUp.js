f6fbad15b58386cc3ac1412eab2683c461e2af83
sylsocha <sylwia9socha@gmail.com>
2024-11-29 11:56:57 +0100

    more SignUp fields

--- frontend/src/js/SignUp.js
+++ frontend/src/js/SignUp.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../css/SignForm.css';
+import CountriesSelect from './CountriesSelect';
 import { Link } from 'react-router-dom';
 
 function SignUp() {
@@ -16,6 +17,42 @@ function SignUp() {
         <div class='formElem'>
           <label for="email">email:</label>
           <input type="email" name='email' required />
+        </div>
+
+        <div class='formElem'>
+          <label for="country">country:</label>
+          <CountriesSelect /> {/*it was way to long to put it in the same file, but might be put back here */}
+        </div>
+
+        <div class='formElem'>
+          <label for="city">city:</label>
+          <input type="text" name='city' required />
+        </div>
+
+        <div class='formElem'>
+          <label for="job_position">job position:</label>
+          <input type="text" name='job_position' required />
+        </div>
+
+        <div class='formElem'>
+          <label for="education_level">education level:</label>
+          <input type="text" name='education_level' required />
+        </div>
+
+        <div class='formElem'>
+          <label for="birth">date of birth:</label>
+          <input type="date" name='birth' required />
+        </div>
+
+        <div class='formElem'>
+          <label for="gerder">gender:</label>
+          <select name="gender" id="gender" required>
+            <option value="">select your gender</option>
+            <option value="F">female</option>
+            <option value="M">male</option>
+            <option value="O">other</option>
+            <option value="N">prefer not to say</option>
+          </select>
         </div>
 
         <div class='formElem'>
