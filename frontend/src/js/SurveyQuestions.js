99079763a2d19877fc9735fce4652ce1ceb3cac0
sylsocha <sylwia9socha@gmail.com>
2024-12-12 10:25:29 +0100

    started templates

--- frontend/src/js/SurveyQuestions.js
+++ frontend/src/js/SurveyQuestions.js
@@ -3,6 +3,8 @@ import '../css/SurveyWindow.css';
 import '../css/Dashboard.css';
 import { Link } from 'react-router-dom';
 import '../css/App.css';
+import Singular from './Questions/Singular';
+import Multiple from './Questions/Multiple';
 
 function SurveyQuestions() {
   const handleLogout = () => {
@@ -19,43 +21,16 @@ function SurveyQuestions() {
               <li><Link to="/dashboard" class='link'>dashboard</Link></li>
             </ul>
           </nav>
-      <template class="question_container">
-        <h3>Question 1</h3> {/*Replace with question number */}
-        <hr class='question_underline'/>
-        <div class="question">
-            <p>question</p> {/*Replace with question */}
-            <div class="singular_choice">
-                <div class="answer">
-                    <input type="radio" id="answer_radioA" name="answer_radio" value="A"/>  {/*Replace value with answer value */}
-                    <label for="answer_radioA">A answer</label>   {/*Replace with answer */}
-                </div>
-                <div class="answer">
-                    <input type="radio" id="answer_radioB" name="answer_radio" value="B"/>  {/*Replace value with answer value */}
-                    <label for="answer_radioB">B answer</label>   {/*Replace with answer */}
-                </div>
-            </div>
-        </div>
-      </template>
-      <div class="question_container">
-        <h3>Question 2</h3> {/*Replace with question number */}
-        <hr class='question_underline'/>
-        <div class="question">
-            <p>question</p> {/*Replace with question */}
-            <div class="multiple_choice">
-                <div class="answer">
-                    <input type="checkbox" id="answer_checkboxA" name="answer_checkbox" value="A"/>   {/*Replace value with answer value */}
-                    <label for="answer_checkboxA">A answer</label>   {/*Replace with answer */}
-                </div>
-                <div class="answer">
-                    <input type="checkbox" id="answer_checkboxB" name="answer_checkbox" value="B"/>   {/*Replace value with answer value */}
-                    <label for="answer_checkboxB">B answer</label>   {/*Replace with answer */}
-                </div>
-            </div>
-        </div>
-      </div>
+      
+      <Singular/>
+
+      <Multiple/>
 
       <div class='finish'>
         <Link to="/thankyou" class='link'><input type="submit" value="finish survey" /></Link>
+      </div>
+      <div class='next_question'>
+        <input type="submit" value="next question" />
       </div>
     </div>
   );
