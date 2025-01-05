c9043590f221c4e65dfbb81f5eeea00851bf0cfc
Rina <doroshukmaryna@gmail.com>
2025-01-05 17:35:26 +0100

    surveyinfo displays info about the survey

--- frontend/src/js/SurveyInfo.js
+++ frontend/src/js/SurveyInfo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import '../css/App.css';
 import '../css/SurveyInfo.css';
 import '../css/SurveyWindow.css';
@@ -8,6 +8,10 @@ import logo from '../images/photos/logo_surveys3.png';
 import bottomImage from '../images/photos/circles.png';
 
 function SurveyInfo() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { survey, user } = location.state;
+
   const logout = async () => {
     try {
       await fetch("http://localhost:8000/logout", {
@@ -19,44 +23,53 @@ function SurveyInfo() {
       console.error("Logout error:", error);
     }
   };
+
+  const formatDateWithRemainingDays = (deadline) => {
+    const deadlineDate = new Date(deadline);
+    const today = new Date();
+    const timeDiff = deadlineDate - today;
+    const daysLeft = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+
+    const options = { year: 'numeric', month: 'short', day: 'numeric' };
+    const formattedDate = deadlineDate.toLocaleDateString('en-US', options);
+
+    return `${formattedDate} (${daysLeft} days left)`;
+  };
+
+  const handleStartSurvey = () => {
+    navigate('/surveywindow', { state: { survey, user } });
+  };
+
   return (
     <div className="SurveyInfo">
       <nav>
-        <ul class='navbar'>
-          <div class='nav_side'>
+        <ul className='navbar'>
+          <div className='nav_side'>
             <li onClick={logout} style={{ cursor: "pointer" }}>Sign out</li> 
-            <li><Link to="/about" class='link'>About</Link></li>
-            <li><Link to="/contact" class='link'>Contact</Link></li>
+            <li><Link to="/about" className='link'>About</Link></li>
+            <li><Link to="/contact" className='link'>Contact</Link></li>
           </div>
           <li><img src={logo} alt='logo'/></li>
-          <div class='nav_side'>
-            <li><Link to="/user" class='link'>User</Link></li>
-            <li><Link to="/dashboard" class='link'>Dashboard</Link></li>
-            <li><Link to="/surveys" class='link'>Surveys</Link></li>
+          <div className='nav_side'>
+            <li><Link to="/user" className='link'>User</Link></li>
+            <li><Link to="/dashboard" className='link'>Dashboard</Link></li>
+            <li><Link to="/surveys" className='link'>Surveys</Link></li>
           </div>
         </ul>
       </nav>
-      <div class='fix_nav_position'/>
-      <div class='survey_info_main_cointainer'>
-          <h2>survey title</h2> {/* from db*/}
-          <hr class='question_underline'></hr>
-          <div class='surv_info_inner_div'>
-            <label>Deadline:</label>
-            <p class='surv_deadline'>deadline</p> {/* from db*/}
-          </div>
-          <div class='surv_info_inner_div'>
-            <label>Issuer:</label>
-            <p>issuer</p> {/* from db*/}
-          </div>
-          <p> survey descripion: In finibus tempus aliquet. Integer porttitor porta malesuada. Praesent lectus risus, porta quis fermentum in, ultricies vitae ipsum. Curabitur felis odio, consequat a eros eu, scelerisque auctor dui. Vivamus maximus viverra orci in vestibulum. Cras fringilla et libero ac lobortis. Aliquam a viverra turpis, sed placerat est. In vel turpis fringilla, suscipit lectus a, pulvinar risus. Ut eget sem placerat, rutrum urna et, bibendum orci. Nulla cursus nunc non diam semper accumsan.
-            Nam in turpis nisl. Integer neque neque, fermentum euismod est a, tempor volutpat magna. Duis vitae orci eu arcu viverra sollicitudin ac at turpis. Maecenas aliquam consectetur nisi, ac mollis quam ultricies in. Interdum et malesuada fames ac ante ipsum primis in faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla fringilla arcu velit, ut fringilla felis elementum id. Maecenas mollis diam nisl, in suscipit purus posuere nec. Donec bibendum, purus vitae blandit porta, arcu nisi tincidunt nibh, eget tempus purus nisi rhoncus metus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent suscipit mi sed sapien fringilla dapibus.
-          </p>
-          <div class='finish survey_info_button'>
-              <Link to="/surveywindow" class='link'><input type="submit" value="Start survey" /></Link>
+      <div className='fix_nav_position'/>
+      <div className='survey_info_main_cointainer'>
+          <h2>{survey.title}</h2>
+          <p><strong>Description:</strong> {survey.description}</p>
+          <p><strong>Deadline:</strong> {formatDateWithRemainingDays(survey.deadline)}</p>
+          <p><strong>Issuer:</strong> {survey.issuer}</p>
+          <p><strong>Points:</strong> {survey.points_awarded}</p>
+          <div className='finish survey_info_button'>
+              <button onClick={handleStartSurvey}>Start survey</button>
           </div>
       </div>
       <div>
-        <img src={bottomImage} alt='circles' class='bottom_image'/>
+        <img src={bottomImage} alt='circles' className='bottom_image'/>
       </div>
 
       <footer>
